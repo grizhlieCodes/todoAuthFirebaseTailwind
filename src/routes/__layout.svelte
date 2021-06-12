6ace539b5bcc4073d3afe36dfa72bd7e3b7db59d
grizhlieCodes <rafal.potasz@gmail.com>
2021-06-12 21:35:01 +0100

    Added firebase init to layout on mount

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,27 +1,55 @@
 <script>
-    import Header from '$lib/Header.svelte'
-    import Signin from '$lib/SignIn.svelte'
+	import Header from '$lib/Header.svelte';
+	import AuthModal from '$lib/AuthModal.svelte';
+	import firebase from 'firebase/app';
+	import UserStore from '../stores/user.js';
+	import 'firebase/auth';
+	import 'firebase/firestore';
+	import { onMount } from 'svelte';
+	let showSigninModal = false;
+	let signInSelected;
 
-    let showSigninModal = false
-    let signInSelected
+	const toggleAuthModal = (e) => {
+		let authType = e.detail === 'signIn' ? true : false;
+		signInSelected = authType;
+		showSigninModal = !showSigninModal;
+	};
 
-    const toggleAuthModal = (e) => {
-        let authType = e.detail === 'signIn' ? true : false
-        signInSelected = authType
-        showSigninModal = !showSigninModal
-    }
+	const setupFirebase = () => {
+		//Only run firebase once
+		if (firebase.apps.length === 0) {
+			let firebaseConfig = {
+				apiKey: 'AIzaSyDBi6ZoZVGOJeDURJ6Kk2xTKw5xQAShJGI',
+				authDomain: 'todo-auth-firebase-tailwind.firebaseapp.com',
+				projectId: 'todo-auth-firebase-tailwind',
+				storageBucket: 'todo-auth-firebase-tailwind.appspot.com',
+				messagingSenderId: '1061500429743',
+				appId: '1:1061500429743:web:156fe8c1af7d2e55b97b5a'
+			};
+			// Initialize Firebase
+			firebase.initializeApp(firebaseConfig);
+
+			firebase.auth().onAuthStateChanged((user) => {
+				UserStore.setUser(user);
+			});
+		}
+	};
+
+	onMount(() => {
+		setupFirebase();
+	});
 </script>
 
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
 
-<Header on:toggleAuthModal={toggleAuthModal}/>
+<Header on:toggleAuthModal={toggleAuthModal} />
 
 {#if showSigninModal}
-     <Signin {signInSelected}/>
+	<AuthModal {signInSelected} on:toggleAuthModal={toggleAuthModal} />
 {/if}
 
-
-<style>
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-</style>
+<slot />
